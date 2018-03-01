ce289651067319a34ec5aaba7117bb57e4a5b6f9
shubhamroy4395 <shubhamroy4395@gmail.com>
2018-03-01 19:50:41 +0530

    second

---
+++ header.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const Header = (props) => {
+  const { textStyle, viewStyle } = styles;
+  return (
+<View style={viewStyle}>
+  <Text style={textStyle}>{props.headerText}</Text>
+</View>
+);
+};
+
+
+const styles = {
+  textStyle: {
+    fontSize: 25,
+    color: '#000'
+  },
+
+  viewStyle: {
+    backgroundColor: '#F8F8F8',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 60,
+    paddingTop: 0,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    elevation: 5,
+    position: 'relative'
+  }
+};
+export default Header;
